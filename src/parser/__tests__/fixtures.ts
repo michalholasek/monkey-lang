3d4a5a9456c11751aa9c8ab8cfbca93de3c0bf03
Michal Holasek <holasek.michal@hotmail.com>
2019-01-10 20:22:34 +0100

    (parser): Use TokenKind enum in test fixtures

--- src/parser/__tests__/fixtures.ts
+++ src/parser/__tests__/fixtures.ts
@@ -16,44 +16,44 @@ export const Statements = {
     statements: [
       {
         expression: {
-          tokens: [{ column: 16, kind: 3, line: 2, literal: '5' }],
+          tokens: [{ column: 16, kind: TokenKind.Int, line: 2, literal: '5' }],
           value: 5
         },
         kind: StatementKind.Let,
         name: { column: 12, kind: 2, line: 2, literal: 'x' },
         tokens: [
-          { column: 8, kind: 17, line: 2, literal: 'let' },
-          { column: 12, kind: 2, line: 2, literal: 'x' },
-          { column: 14, kind: 4, line: 2, literal: '=' },
-          { column: 16, kind: 3, line: 2, literal: '5' }
+          { column: 8, kind: TokenKind.Let, line: 2, literal: 'let' },
+          { column: 12, kind: TokenKind.Identifier, line: 2, literal: 'x' },
+          { column: 14, kind: TokenKind.Assign, line: 2, literal: '=' },
+          { column: 16, kind: TokenKind.Int, line: 2, literal: '5' }
         ]
       },
       {
         expression: {
-          tokens: [{ column: 16, kind: 3, line: 3, literal: '10' }],
+          tokens: [{ column: 16, kind: TokenKind.Int, line: 3, literal: '10' }],
           value: 10
         },
         kind: StatementKind.Let,
-        name: { column: 12, kind: 2, line: 3, literal: 'y' },
+        name: { column: 12, kind: TokenKind.Identifier, line: 3, literal: 'y' },
         tokens: [
-          { column: 8, kind: 17, line: 3, literal: 'let' },
-          { column: 12, kind: 2, line: 3, literal: 'y' },
-          { column: 14, kind: 4, line: 3, literal: '=' },
-          { column: 16, kind: 3, line: 3, literal: '10' }
+          { column: 8, kind: TokenKind.Let, line: 3, literal: 'let' },
+          { column: 12, kind: TokenKind.Identifier, line: 3, literal: 'y' },
+          { column: 14, kind: TokenKind.Assign, line: 3, literal: '=' },
+          { column: 16, kind: TokenKind.Int, line: 3, literal: '10' }
         ]
       },
       {
         expression: {
-          tokens: [{ column: 21, kind: 3, line: 4, literal: '838383' }],
+          tokens: [{ column: 21, kind: TokenKind.Int, line: 4, literal: '838383' }],
           value: 838383
         },
         kind: StatementKind.Let,
         name: { column: 12, kind: 2, line: 4, literal: 'foobar' },
         tokens: [
-          { column: 8, kind: 17, line: 4, literal: 'let' },
-          { column: 12, kind: 2, line: 4, literal: 'foobar' },
-          { column: 19, kind: 4, line: 4, literal: '=' },
-          { column: 21, kind: 3, line: 4, literal: '838383' }
+          { column: 8, kind: TokenKind.Let, line: 4, literal: 'let' },
+          { column: 12, kind: TokenKind.Identifier, line: 4, literal: 'foobar' },
+          { column: 19, kind: TokenKind.Assign, line: 4, literal: '=' },
+          { column: 21, kind: TokenKind.Int, line: 4, literal: '838383' }
         ]
       }
     ]
@@ -62,35 +62,35 @@ export const Statements = {
     statements: [
       {
         expression: {
-          tokens: [{ column: 15, kind: 3, line: 2, literal: '5' }],
+          tokens: [{ column: 15, kind: TokenKind.Int, line: 2, literal: '5' }],
           value: 5
         },
         kind: StatementKind.Return,
         tokens: [
-          { column: 8, kind: 22, line: 2, literal: 'return' },
-          { column: 15, kind: 3, line: 2, literal: '5' }
+          { column: 8, kind: TokenKind.Return, line: 2, literal: 'return' },
+          { column: 15, kind: TokenKind.Int, line: 2, literal: '5' }
         ]
       },
       {
         expression: {
-          tokens: [{ column: 15, kind: 3, line: 3, literal: '10' }],
+          tokens: [{ column: 15, kind: TokenKind.Int, line: 3, literal: '10' }],
           value: 10
         },
         kind: StatementKind.Return,
         tokens: [
-          { column: 8, kind: 22, line: 3, literal: 'return' },
-          { column: 15, kind: 3, line: 3, literal: '10' }
+          { column: 8, kind: TokenKind.Return, line: 3, literal: 'return' },
+          { column: 15, kind: TokenKind.Int, line: 3, literal: '10' }
         ]
       },
       {
         expression: {
-          tokens: [{ column: 15, kind: 3, line: 4, literal: '993322' }],
+          tokens: [{ column: 15, kind: TokenKind.Int, line: 4, literal: '993322' }],
           value: 993322
         },
         kind: StatementKind.Return,
         tokens: [
-          { column: 8, kind: 22, line: 4, literal: 'return' },
-          { column: 15, kind: 3, line: 4, literal: '993322' }
+          { column: 8, kind: TokenKind.Return, line: 4, literal: 'return' },
+          { column: 15, kind: TokenKind.Int, line: 4, literal: '993322' }
         ]
       }
     ]
@@ -102,11 +102,11 @@ export const Expressions = {
     statements: [
       {
         expression: {
-          tokens: [{ column: 2, kind: 2, line: 1, literal: 'foobar' }],
+          tokens: [{ column: 2, kind: TokenKind.Identifier, line: 1, literal: 'foobar' }],
           value: 'foobar'
         },
         kind: StatementKind.Expression,
-        tokens: [{ column: 2, kind: 2, line: 1, literal: 'foobar' }]
+        tokens: [{ column: 2, kind: TokenKind.Identifier, line: 1, literal: 'foobar' }]
       }
     ]
   },
@@ -114,11 +114,11 @@ export const Expressions = {
     statements: [
       {
         expression: {
-          tokens: [{ column: 2, kind: 3, line: 1, literal: '5' }],
+          tokens: [{ column: 2, kind: TokenKind.Int, line: 1, literal: '5' }],
           value: 5
         },
         kind: StatementKind.Expression,
-        tokens: [{ column: 2, kind: 3, line: 1, literal: '5' }]
+        tokens: [{ column: 2, kind: TokenKind.Int, line: 1, literal: '5' }]
       }
     ]
   },
